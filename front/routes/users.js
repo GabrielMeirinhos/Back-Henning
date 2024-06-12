b023e72f583fc084cedc7b4eaf2c5ba24715a667
Gabriel Meirinhos <128100486+GabrielMeirinhos@users.noreply.github.com>
2024-06-12 18:35:35 +0000

    feat: criação da TELA DE LOGIN e funcionalidades

--- front/routes/users.js
+++ front/routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+
+var express = require('express');
+var router = express.Router();
+const url = "https://animated-lamp-6j9gw469wr62x5jg-4000.app.github.dev/users/"
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  fetch(url, { method: 'GET' })
+    .then(async (res) => {
+      if (!res.ok) {
+        const err = await res.json()
+        throw err
+      }
+      return res.json()
+    })
+    .then((users) => {
+      let title = "Tela De Login"
+      let cols = ["ID", "Usuário", "Sennha", "E-Mail", "Saldo R$"]
+      res.render('users', { title, users, cols, error: "" }) 
+    })
+    .catch((error) => {
+      console.log('Erro', error)
+      res.render('users', { title: "Gestão de Usuários", error: "" })
+    })
 });
+// POST new user
+router.post("/", (req, res) => {
+  const { username, password, email, saldo } = req.body
+  fetch(url + '/register', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password, email, saldo })
+  }).then(async (res) => {
+    if (!res.ok) {
+      const err = await res.json()
+      throw err
+    }
+    return res.json()
+  })
+    .then((user) => {
+      res.send(user)
+    })
+    .catch((error) => {
+      res.status(500).send(error)
+    })
+})
 
 module.exports = router;
